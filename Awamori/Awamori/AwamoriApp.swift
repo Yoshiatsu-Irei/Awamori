ca17f3877429da3257b7208b78a116bf2891f7b1
Yoshiatsu-Irei <yoshiatsu.irei@fuller.co.jp>
2020-10-31 17:22:24 +0900

    first commit

---
+++ Awamori/Awamori/AwamoriApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwamoriApp.swift
+//  Awamori
+//
+//  Created by 伊禮吉充 on 2020/10/31.
+//
+
+import SwiftUI
+
+@main
+struct AwamoriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Awamori/Awamori/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Awamori
+//
+//  Created by 伊禮吉充 on 2020/10/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
